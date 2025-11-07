ebbdeec09ce269ccc8c6bdf6c171a27c02d5abc1
HUNGNAMHOMEPC\truon <truongnhuhungnam@gmail.com>
2025-11-07 08:24:35 +0700

    Add post detail loader and display in posts.$id route

--- src/routes/posts.$id.tsx
+++ src/routes/posts.$id.tsx
@@ -1,11 +1,28 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, notFound } from "@tanstack/react-router";
+import { getPostById } from "@/server/posts";
 
 export const Route = createFileRoute("/posts/$id")({
 	component: RouteComponent,
+	loader: async ({ params }) => {
+		const post = await getPostById({ data: params.id });
+
+		if (!post) {
+			throw notFound();
+		}
+
+		return post;
+	},
 });
 
 function RouteComponent() {
-	const { id } = Route.useParams();
+	const post = Route.useLoaderData();
 
-	return <div>Hello "/posts/{id}"!</div>;
+	return (
+		<div>
+			<article className="space-y-8">
+				<h1 className="text-3xl font-bold text-slate-900">{post.title}</h1>
+				<p className="whitespace-pre-line text-slate-800">{post.content}</p>
+			</article>
+		</div>
+	);
 }
--- src/server/posts.ts
+++ src/server/posts.ts
@@ -7,3 +7,27 @@ export const getPosts = createServerFn().handler(async () => {
 	const posts = await prisma.post.findMany();
 	return posts;
 });
+
+export const getPostById = createServerFn()
+	.inputValidator((id: unknown) => {
+		if (typeof id === "string" && id.length > 0) {
+			return id;
+		}
+
+		return null;
+	})
+	.handler(async ({ data: id }) => {
+		if (!id) {
+			return null;
+		}
+
+		const post = await prisma.post.findUnique({
+			where: { id },
+		});
+
+		if (!post) {
+			return null;
+		}
+
+		return post;
+	});
